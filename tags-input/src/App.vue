29a53925dc9c058ce1874738ad0fa2d7369d52f7
Bryan <54081465+arthvadrr@users.noreply.github.com>
2024-10-04 14:25:31 -0500

    Vue tag-input

--- tags-input/src/App.vue
+++ tags-input/src/App.vue
@@ -1,22 +1,26 @@
 <script>
-import TagsInput from './components/TagsInput.vue';
+	import TagsInput from './components/TagsInput.vue';
 
-export default {
-  components: {
-    TagsInput,
-  },
-};
+	export default {
+		data: () => ({
+			jsFrameworks: ['react.js', 'vue.js', 'angular.js'],
+		}),
+		mounted() {},
+		components: {
+			TagsInput,
+		},
+	};
 </script>
 
 <template>
-  <div>
-    <h1>Hello World</h1>
-    <TagsInput />
-  </div>
+	<div>
+		<h1>Hello World</h1>
+		<TagsInput :selectedTags="jsFrameworks" />
+	</div>
 </template>
 
 <style>
-h1 {
-  text-shadow: 1px 1px 1px #ff0000;
-}
+	h1 {
+		text-shadow: 1px 1px 1px #ff0000;
+	}
 </style>
--- tags-input/src/components/TagsInput.vue
+++ tags-input/src/components/TagsInput.vue
@@ -1,17 +1,112 @@
+<script>
+	export default {
+		data() {
+			return {
+				tags: [...this.selectedTags],
+				newTag: '',
+			};
+		},
+		props: {
+			selectedTags: {
+				type: Array,
+				required: true,
+				default: '',
+			},
+		},
+		watch: {
+			newTag(newVal, oldVal) {
+				if (newVal.indexOf(',') > -1) {
+					this.newTag = oldVal;
+					this.addNewTag();
+				}
+			},
+		},
+		computed: {
+			isTagExists() {
+				return this.tags.includes(this.newTag);
+			},
+		},
+		methods: {
+			addNewTag() {
+				if (
+					typeof this.newTag === 'string' &&
+					this.newTag.length > 0 &&
+					!this.isTagExists
+				) {
+					this.tags.push(this.newTag);
+					this.newTag = '';
+				}
+			},
+			removeTag(index) {
+				if (this.tags[index]) {
+					this.tags.splice(index, 1);
+				}
+			},
+			removeLastTag() {
+				if (this.tags.length) {
+					this.tags.pop();
+				}
+			},
+			getInputStyles() {
+				let styles = '';
+
+				if (this.isTagExists) {
+					styles += 'color: #FF0000;';
+					styles += 'text-decoration: line-through;';
+				}
+
+				return styles;
+			},
+		},
+	};
+</script>
+
 <template>
-  <div>
-    <div v-for="(tag, index) in tags" v-bind:key="index">
-      <span>{{ tag + index }}</span>
-    </div>
-    <input type="text" :value="newTag" v-on:keydown.enter="tags.push($event.target.value)" />
-  </div>
+	<div class="tags-wrapper">
+		selected tags: {{ selectedTags }}
+		<div
+			v-for="(tag, index) in tags"
+			v-bind:key="index"
+			class="tags-list"
+		>
+			<div class="tag-item">
+				<hr />
+				<span>{{ `${String(index).padStart(3, '0')} : ${tag}` }}</span>
+				<button
+					@click="removeTag(index)"
+					href="#"
+					aria-label="Remove Tag"
+					style="float: right"
+				>
+					&times;
+				</button>
+			</div>
+		</div>
+		<input
+			@keydown.enter="addNewTag($event)"
+			@keydown.tab.prevent="addNewTag"
+			:style="getInputStyles()"
+			:class="{ 'tag-exists': isTagExists }"
+			class="tag-input"
+			v-model.trim="newTag"
+			type="text"
+		/>
+		<button
+			@click="removeLastTag"
+			:disabled="this.tags.length < 1"
+			href="#"
+			aria-label="Undo"
+			style="float: right"
+			class="remove-tag"
+		>
+			Remove Last
+		</button>
+	</div>
 </template>
 
-<script>
-export default {
-  data: () => ({
-    tags: ['vue', 'react', 'angular'],
-    newTag: 'preact',
-  }),
-};
-</script>
+<style scoped>
+	.tag-exists {
+		color: #ff0000;
+		text-decoration: line-through;
+	}
+</style>
